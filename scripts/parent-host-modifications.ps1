c142c0e3c5c8d33cbf2bb1c34aa759c168e29a86
Kalen <kalenarndt@gmail.com>
2020-10-19 16:02:05 -0600

    Host Script
    Added non-nested host script to disable salting, large pages, and enable intravm hyperthreading.

---
+++ scripts/parent-host-modifications.ps1
@@ -0,0 +1,26 @@
+#
+# Configures homelab to disable huge pages, salting, and allows hyperthreading between vms
+# 
+#
+
+param(
+   [string]$vCenter = "vc.bmrf.io",
+   [string]$username = "administrator@vsphere.local",
+   [string]$password = "VMware123!"
+)
+
+Connect-VIServer -Server $vCenter -User $username -Password $password
+# we should specify either -s or -o
+
+
+Connect-VIServer vc.bmrf.io
+
+$esxHosts = Get-VMHost | Sort Name
+foreach($esx in $esxHosts){
+      Write-Host "Disabling PageShare Salting on $esx"
+      Get-AdvancedSetting -Entity $esx -Name Mem.ShareForceSalting | Set-AdvancedSetting -Value 0 -Confirm:$false | Out-Null
+      Write-Host "Disabling Large Pages on $esx"
+      Get-AdvancedSetting -Entity $esx -Name Mem.AllocGuestLargePage | Set-AdvancedSetting -Value 0 -Confirm:$false | Out-Null
+      Write-Host "Enabling IntraVM Hyperthreading on $esx"
+      Get-AdvancedSetting -Entity $esx -Name VMkernel.Boot.hyperthreadingMitigationIntraVM | Set-AdvancedSetting -Value false -Confirm:$false | Out-Null   
+   }
