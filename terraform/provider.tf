6f886b421551cdbaeacdc0d4d8f654535f0ac5ec
Kalen <kalenarndt@gmail.com>
2021-02-24 13:30:19 -0700

    Git ignore changes
    Added provider.tf file for the actual provider that is used.
    
    Modified gitignore to exclude tfvars,hcl,and anything in the .terraform folder.

---
+++ terraform/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    nsxt = {
+      source  = "vmware/nsxt"
+      version = "3.1.1"
+    }
+  }
+}
+
+# NSX-T Manager Credentials
+provider "nsxt" {
+  host                  = var.nsx_manager
+  username              = var.username
+  password              = var.password
+  allow_unverified_ssl  = true
+  max_retries           = 10
+  retry_min_delay       = 500
+  retry_max_delay       = 5000
+  retry_on_status_codes = [429]
+}
+s
