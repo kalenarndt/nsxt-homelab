196a7575f175d9a7acdd675373de4dcb608b8767
Kalen <kalenarndt@gmail.com>
2021-03-03 07:57:27 -0700

    Updated parent-host-modifications
    Removed line for large pages as hosts that I have now aren't memory bound.

--- scripts/parent-host-modifications.ps1
+++ scripts/parent-host-modifications.ps1
@@ -13,14 +13,10 @@ Connect-VIServer -Server $vCenter -User $username -Password $password
 # we should specify either -s or -o
 
 
-Connect-VIServer vc.bmrf.io
-
 $esxHosts = Get-VMHost | Sort Name
 foreach($esx in $esxHosts){
       Write-Host "Disabling PageShare Salting on $esx"
       Get-AdvancedSetting -Entity $esx -Name Mem.ShareForceSalting | Set-AdvancedSetting -Value 0 -Confirm:$false | Out-Null
-      Write-Host "Disabling Large Pages on $esx"
-      Get-AdvancedSetting -Entity $esx -Name Mem.AllocGuestLargePage | Set-AdvancedSetting -Value 0 -Confirm:$false | Out-Null
       Write-Host "Enabling IntraVM Hyperthreading on $esx"
       Get-AdvancedSetting -Entity $esx -Name VMkernel.Boot.hyperthreadingMitigationIntraVM | Set-AdvancedSetting -Value false -Confirm:$false | Out-Null   
    }
